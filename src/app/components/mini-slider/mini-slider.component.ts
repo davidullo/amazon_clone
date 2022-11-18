b52a1e03af931ec21d3cb04ca191ec7da18f1d3e
Davide Barbieri <dbarbieri@red.software.systems>
2022-11-18 17:42:37 +0100

    removing some comments

--- src/app/components/mini-slider/mini-slider.component.ts
+++ src/app/components/mini-slider/mini-slider.component.ts
@@ -49,9 +49,9 @@ export class MiniSliderComponent implements OnInit {
   }
 
   shuffleArray(array: Minislider[]) {
-    for (var i = array.length - 1; i > 0; i--) {
-      var j = Math.floor(Math.random() * (i + 1));
-      var temp = array[i];
+    for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      let temp = array[i];
       array[i] = array[j];
       array[j] = temp;
     }
